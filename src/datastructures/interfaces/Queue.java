254caf3713b1133f4515222bf5d5a6f760d4d5b2
Macsaroo <sajjad.sarookhani@gmail.com>
2025-05-30 14:46:35 +0330

    fix some bug in main project

--- src/datastructures/interfaces/Queue.java
+++ src/datastructures/interfaces/Queue.java
@@ -1,21 +1,12 @@
 package datastructures.interfaces;
 
-import java.util.Collection;
-
 public interface Queue<T> {
-    boolean add(Object t);
-
-    boolean offer(Object t);
-
+    boolean add(T t);
+    boolean offer(T t);
     T remove();
-
     T poll();
-
     T element();
-
     T peek();
-
     boolean isEmpty();
-
     int size();
 }
