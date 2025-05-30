52ba49c5cc651f8b4617aca2d22abaf20fc143ec
Macsaroo <sajjad.sarookhani@gmail.com>
2025-05-30 14:27:25 +0330

    write CustomQueue :)

--- src/datastructures/queues/CustomQueue.java
+++ src/datastructures/queues/CustomQueue.java
@@ -4,12 +4,62 @@ import datastructures.interfaces.Queue;
 import datastructures.lists.CustomLinkedList;
 
 public class CustomQueue<T> implements Queue<T> {
+
     private CustomLinkedList<T> list;
 
     public CustomQueue() {
-        list = new CustomLinkedList<>();
+        this.list = new CustomLinkedList<>();
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public boolean add(T t) {
+        list.addLast(t);
+        return true;
+    }
 
+    @Override
+    public boolean offer(T t) {
+        return add(t);
+    }
+
+    @Override
+    public T remove() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public T poll() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.removeFirst();
+    }
+
+    @Override
+    public T element() {
+        return list.getFirst();
+    }
+
+    @Override
+    public T peek() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.getFirst();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
 }
